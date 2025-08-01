02fa2d92575734bde07f6f4197c2c089f6fae8e3
biggerbomber <barbato.alberto07@gmail.com>
2025-08-01 22:11:28 +0200

    V1- un po inefficente ma meh funziona

---
+++ gen_smooot.py
@@ -0,0 +1,40 @@
+from Crypto.Util.number import getPrime,isPrime
+
+
+
+
+
+def getSmootPrime(nbits,cutoffbits=10):
+    if nbits <=cutoffbits:
+        return getPrime(nbits)
+
+    while True:
+        primes = [getPrime(cutoffbits) for _ in range(nbits//cutoffbits)]
+        if nbits%cutoffbits>2:
+            primes+=[getPrime(nbits%cutoffbits)] 
+        tot = 2
+        for p in primes:
+            tot*=p
+        
+        if isPrime(tot+1):
+            return tot+1
+
+    
+
+'''
+EXAMPLE
+import random as rnd
+from sage.all import *
+print(factor(getSmootPrime(512)-1))
+
+p =  getSmootPrime(1512)
+
+a = Mod(rnd.randint(3,p),p)
+
+g = Mod(2,p)
+
+b = pow(g,a,p)
+
+print(a)
+print(b.log(g))
+'''
